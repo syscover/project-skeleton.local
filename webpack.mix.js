411f7ebd0c8f5f08e941ac342970b74b432979db
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-06-09 00:27:13 +0200

    build

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,13 +16,18 @@ mix
     .js('resources/assets/js/app.js', 'public/js')
     .styles([
         'node_modules/swiper/dist/css/swiper.min.css',
+        'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/animate.css/animate.min.css'
     ], 'public/css/all.css')
     .scripts([
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/popper.js/dist/umd/popper.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/moment/moment.js',
+        'node_modules/jquery-validation/dist/jquery.validate.js',
+        'node_modules/jquery-validation/dist/additional-methods.js',
         'node_modules/swiper/dist/js/swiper.min.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
     ], 'public/js/all.js')
     .options({
         processCssUrls: true
